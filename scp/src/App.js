59ad0c262b9078d3f1a55d90f6c78ffe3492f2e8
Regan <regantaigel@gmail.com>
2022-09-12 10:10:10 +1200

    Deployed website

--- scp/src/App.js
+++ scp/src/App.js
@@ -2,12 +2,14 @@ import Content from './Content.json';
 
 const SCP = Content.map(
 (content) => {return(
-  <div> 
-    <h1>Item: {content.item}</h1>
-    <h3>Class: {content.class}</h3>
-    <p>Contaiment:{content.contaiment}</p>
-    <p> {content.discrpion}</p>
-    <p><img src={content.images} alt="pic"></img></p>
+  <div className = "bg-dark p-3 card m-3"> 
+    
+    <h1 className='text-white card-black text-center bg-primary'>Item: {content.item}</h1>
+    <h3 className='text-white card-black text-center bg-danger'>Class: {content.class}</h3>
+    <p className='text-white card-black m-3'>Contaiment: {content.contaiment}</p>
+    <p className='text-white card-black m-3'> {content.discrpion}</p>
+    <p><img src={content.images} alt="Pic" className='img-fluid rounded mx-auto d-block b m-3'></img></p>
+    
   </div>);}
 )
 
